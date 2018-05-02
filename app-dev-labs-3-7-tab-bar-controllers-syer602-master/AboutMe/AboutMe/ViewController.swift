63c831b2717142db43be7de43ceaf3df2cb34857
Steven <sdong1@live.maryville.edu>
2018-05-02 14:49:01 -0500

    Change

---
+++ app-dev-labs-3-7-tab-bar-controllers-syer602-master/AboutMe/AboutMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Gross, Paul on 10/30/17.
+//  Copyright © 2017 Maryville App Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
